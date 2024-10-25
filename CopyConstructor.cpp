57a0fabd91dead74635e272a6f1abcd8488457b0
Venugopal K <venu.shastri@gmail.com>
2024-10-25 15:51:48 +0530

    Create CopyConstructor.cpp

---
+++ CopyConstructor.cpp
@@ -0,0 +1,33 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+class B{
+    int _state;
+    public:
+     void setState(int state ){
+         this->_state=state;
+     }
+     int getState(){ return this->_state;}
+};
+class A{
+    B bInstance;
+    public :
+    A(int state){
+        this->bInstance.setState(state);
+    }
+    int getState(){
+        return this->bInstance.getState();
+    }
+    void setState(int newValue){ this->_bInstance.setState(newValue);}
+};
+int main() {
+    // Write C++ code here
+    A aInstance{ 100};
+    std::cout <<aInstance.getState()<<std::endl;
+    
+    A newInstance(aInstance);//Copy Constructor
+    std::cout <<newInstance.getState()<<std::endl;
+    newInstance.setState(500);
+    std::cout <<"aInstance"<<aInstance.getState()<<std::endl;
+    std::cout <<"newInstance"<<newInstance.getState()<<std::endl;
+    return 0;
+}
